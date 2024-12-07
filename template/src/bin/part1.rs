620cae78a2d9a4e0baad4f14661a84a8c39f3396
Kingsley Yung <kingsley@kkoyung.dev>
2024-12-07 12:57:14 +0800

    New template

---
+++ template/src/bin/part1.rs
@@ -0,0 +1,25 @@
+use std::fs;
+use template::*;
+
+fn main() {
+    println!("Part 1");
+
+    let input = fs::read_to_string("input").expect("Unable to read file");
+    println!("{}", process(input.as_str()));
+}
+
+fn process(input: &str) -> usize {
+    // Write here
+    input.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::process;
+    use template::INPUT;
+
+    #[test]
+    fn test_example() {
+        assert_eq!(process(INPUT), 0);
+    }
+}
